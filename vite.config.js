f95f90432eb6dcd08719d08ad29264aef249b081
kunal639 <kunal.anand639@gmail.com>
2025-09-01 23:52:08 +0530

    setup gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio-Kunal/',
   server: {
     host: '0.0.0.0',
   }
